598f6afb3179d939a25f679a8ec6607b579037c4
tommyc2 <20101841@mail.wit.ie>
2024-11-25 10:33:51 +0000

    Random Number generation 3 arrays 60% section

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # =======================================================
-# Tommy Condon
+# Name: Tommy Condon
 # Student Number: 20101841
-# Lotto API Assignment
-# Module: Embedded Systems Scripting Year 3
+# National Lotto Assignment
+# Module: Embedded Systems Scripting Year 3#
+# Course: Applied Computing Year 3
 # =======================================================
 
 import urllib.request
@@ -10,9 +11,11 @@ import random
 import re
 from time import sleep
 
-user_number_list = []
-winning_numbers_list = []
-bonus_numbers = [0,0]
+user_number_list = [] # users quick pick/manual entries
+winning_numbers_list = [] # stores winning lotto numbers
+bonus_numbers = [0,0] # stores bonus (winning and user one) numbers 
+lotto_plus_1 = []
+lotto_plus_2 = []
 
 # Menu function
 # Main function for displaying menu and options
@@ -20,6 +23,8 @@ def menu():
     global user_number_list
     global winning_numbers_list
     global bonus_numbers
+    global lotto_plus_2
+    global lotto_plus_1
 
     print("===================")
     print("==== Main Menu ====")
@@ -30,29 +35,38 @@ def menu():
 
     choice = int(input("--->    "))
 
+    lotto_plus = input("Do you want to be entered into lotto plus? (Y / N): ")
+
     match choice:
         case 1:
-            print("==================================")
+            print("\n==================================")
             print("======= Quick Pick Mode ==========")
             print("==================================\n")
 
             user_number_list = quick_pick()
 
-            print("Your quick pick:")
-            print(user_number_list, "YOUR BONUS NUMBER: ",bonus_numbers[0],"\n", end=" ")
+            print("Your quick pick:",user_number_list, "YOUR BONUS NUMBER: ",bonus_numbers[0],"\n")
 
             sleep(1)
 
-            try:
-                winning_numbers_list = get_draw_result()
-            except Exception as error:
-                print("API request error",error)
+            #winning_numbers_list = get_draw_result()
+            winning_numbers_list = random.sample(range(1,47),7)
+                
+            if lotto_plus == 'Y' or lotto_plus == 'y':
+                lotto_plus_1 = random.sample(range(1,47),7)
+                lotto_plus_2 = random.sample(range(1,47),7)
+                
+            #bonus_numbers[1] = 7
+            print("Lotto Winning Numbers: ", winning_numbers_list[:-1], "--> Bonus Num: ", winning_numbers_list[6])
+            if lotto_plus == "Y" or lotto_plus == "y":
+                print("Lotto Plus 1 Winning Numbers", lotto_plus_1[:-1], "--> Bonus Num: ", lotto_plus_1[6])
+                print("Lotto Plus 2 Winning Numbers", lotto_plus_2[:-1], "--> Bonus Num: ", lotto_plus_2[6],"\n")
 
-            print("\n=============================")
-            print("====== Prizes won ==========")
-            print("============================\n")
+            #print("\n=============================")
+            #print("====== Prizes won ==========")
+            #print("============================\n")
 
-            get_prizes(user_number_list,winning_numbers_list)
+            #get_prizes(user_number_list,winning_numbers_list)
             print("\n")
             
         case 2:
@@ -62,23 +76,30 @@ def menu():
 
             user_number_list = manual_entry()
 
-            print("Your numbers picked:")
-            print(user_number_list, "YOUR BONUS NUMBER: ",bonus_numbers[0],"\n", end=" ")
+            print("Your numbers picked: ",user_number_list, "YOUR BONUS NUMBER: ",bonus_numbers[0],"\n")
 
             sleep(1)
 
-            try:
-                winning_numbers_list = get_draw_result()
-            except Exception as error:
-                print("API request error",error)
+            #winning_numbers_list = get_draw_result()
+            winning_numbers_list = random.sample(range(1,47),7)
+                
+            if lotto_plus == 'Y' or lotto_plus == 'y':
+                lotto_plus_1 = random.sample(range(1,47),7)
+                lotto_plus_2 = random.sample(range(1,47),7)
+                
+            #bonus_numbers[1] = 7
+            print("Lotto Winning Numbers: ", winning_numbers_list[:-1], "--> Bonus Num: ", winning_numbers_list[6])
+            if lotto_plus == "Y" or lotto_plus == "y":
+                print("Lotto Plus 1 Winning Numbers", lotto_plus_1[:-1], "--> Bonus Num: ", lotto_plus_1[6])
+                print("Lotto Plus 2 Winning Numbers", lotto_plus_2[:-1], "--> Bonus Num: ", lotto_plus_2[6],"\n")
 
-            print("============================")
-            print("====== Prizes won ==========")
-            print("============================\n")
+            #print("============================")
+            #print("====== Prizes won ==========")
+            #print("============================\n")
 
-            get_prizes(user_number_list,winning_numbers_list)
+            #get_prizes(user_number_list,winning_numbers_list)
 
-
+# Print out prizes given the users numbers (e.g. quick pick or manual) and the last draw
 def get_prizes(users_list,winning_numbers):
     global bonus_numbers
 
@@ -88,9 +109,9 @@ def get_prizes(users_list,winning_numbers):
     is_bonus_matched = False
 
     if (bonus_numbers[0] == bonus_numbers[1]):
-        is_bonus_matched = True
+        is_bonus_matched = True # setting bonus match to true if bonus number is matched
     else:
-        is_bonus_matched = False
+        is_bonus_matched = False # otherwise, false
 
     if num_of_matches == 0:
         print("\nSorry, you didn't win anything. Do try again though!")
@@ -101,13 +122,19 @@ def get_prizes(users_list,winning_numbers):
 
         if (num_of_matches == 6):
             print("you've won the jackpot")
-        elif (num_of_matches == 4) or (num_of_matches == 5):
+        elif (num_of_matches == 5):
+            print("You've won cash prize")
+        elif (num_of_matches == 4):
             print("You've won cash prize")
         elif (num_of_matches == 3):
-            print("You've won a scratch card")
+            if (is_bonus_matched):
+                print("You've won cash prize")
+            else:
+                print("You've won a scratch card")
         else:
             print("Sorry, you've won nothing, maybe better luck next time!")
 
+# Grab the last set of draw results from the API
 def get_draw_result():
     global bonus_numbers
 
@@ -127,13 +154,21 @@ def get_draw_result():
 
     winning_nums = []
 
+    # Random Number Part
+    #for i in range (6):
+    #    if (len(winning_nums) == 6):
+    #        bonus_numbers[1] = random.randint(1,47)
+    #    else:
+    #        num = random.randint(1,47)
+     #       winning_nums.append(num)
+
     for num in lottoResults:
         if (len(winning_nums) == 6):
             bonus_numbers[1] = lottoResults[6]
         else:
             winning_nums.append(int(num))
 
-    print("W: ", winning_nums, end=", ")
+    print("Winning Draw: ", winning_nums, end=", ")
     print("Bonus No.: ", bonus_numbers[1])
 
     return winning_nums
@@ -141,25 +176,29 @@ def get_draw_result():
 def generate_bonus_number():
     return random.randint(1,47)
 
-
+# Generate a quick pick card and return the numbers generated (1-47)
 def quick_pick():
     global bonus_numbers
 
     qp_numbers = []
     for n in range(6):
         random_num = random.randint(1,47)
-        if (random_num not in qp_numbers) and (is_valid_lotto_number(random_num)): # double checking that random integer generated is in fact 1-47
+        if (random_num not in qp_numbers):
             qp_numbers.append(random_num)
+        else:
+            qp_numbers.append(random.randint(1,47))
     bonus_numbers[0] = generate_bonus_number()
 
     return qp_numbers
 
+# Check if the user's entered number for manual mode is between 1-47. Otherwise, return false
 def is_valid_lotto_number(chosen_num):
     if (chosen_num <= 47) and (chosen_num >= 1):
         return True
     else:
         return False
 
+# Manual mode for lotto interface
 def manual_entry():
     global bonus_numbers
     
@@ -172,4 +211,5 @@ def manual_entry():
     
     return nums_chosen # list of numbers manually entered excluding bonus number
 
-menu()
+
+menu() # Calling menu function to start script
--- test.py
+++
@@ -1,5 +0,0 @@
-def hello(**args):
-    for val in args.values():
-        print(val)
-
-hello(val1="hello", val2="Tommy")
